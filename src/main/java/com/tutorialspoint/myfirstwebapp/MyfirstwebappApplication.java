f087ac7efefeffdca9879d09828db07da6f0f090
Athar <athar1991pak@yahoo.com>
2020-04-02 18:41:02 +0200

    user rest template to consume a webservice

--- src/main/java/com/tutorialspoint/myfirstwebapp/MyfirstwebappApplication.java
+++ src/main/java/com/tutorialspoint/myfirstwebapp/MyfirstwebappApplication.java
@@ -2,12 +2,28 @@ package com.tutorialspoint.myfirstwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class MyfirstwebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyfirstwebappApplication.class, args);
+	}
+
+
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Hello World";
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
